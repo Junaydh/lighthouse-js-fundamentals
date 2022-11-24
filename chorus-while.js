e108b3fd8934b24eebdf7e07175ac5d7e7452385
Junayd H <junaydhus@gmail.com>
2022-11-24 12:21:08 -0800

    updated formatting

--- chorus-while.js
+++ chorus-while.js
@@ -1,4 +1,4 @@
-//while loops
+//While loops
 
 //example 1:
 
@@ -11,15 +11,18 @@
 // }
 // console.log("Until the sun comes up!");
 
-const chorus = "Let's dance!";
-let repeat = 0;
 
-while (repeat < 10) {
-  if (repeat === 5) {
-    console.log("*change key*")
-  }
-  console.log(chorus);
-  repeat++;
-}
+//example 2:
 
-console.log("Until the sun comes up!")
+// const chorus = "Let's dance!";
+// let repeat = 0;
+
+// while (repeat < 10) {
+//   if (repeat === 5) {
+//     console.log("*change key*")
+//   }
+//   console.log(chorus);
+//   repeat++;
+// }
+
+// console.log("Until the sun comes up!")
