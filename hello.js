f002d0684e1af7f8f8400e862a326cdfe079f297
Junayd H <junaydhus@gmail.com>
2022-11-26 15:22:41 -0800

    wrote a function to say hello using console.log

---
+++ hello.js
@@ -0,0 +1,20 @@
+//sayHello function
+
+//example 1
+
+// const sayHello = function () {
+//   console.log('Hello, World!');
+// }
+
+// sayHello();
+
+
+//example 2
+
+const sayHello = function (name) {
+  console.log(`Hello, ${name}`);
+}
+
+sayHello('Caliban');
+sayHello('Miranda');
+sayHello('Ferdinand');
