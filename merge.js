e24419c05d6831789da73da1362ccafee60527ed
Junayd H <junaydhus@gmail.com>
2022-11-29 13:22:04 -0800

    made formatting changes to concat.js and created a function called merge to concatenate and then sort 2 arrays

--- merge.js
+++ merge.js
@@ -1,10 +1,10 @@
-function concat(arr1, arr2) {
+function merge(arr1, arr2) {
   let finalArr = arr1;
   if (arr2.length > 0) {
     for (let i = 0; i < arr2.length; i++) {
       finalArr.push(arr2[i]);
+    }
+    return finalArr.sort();
   }
-  return finalArr;
-}
-return finalArr
+  return finalArr.sort()
 }
